df81e3964cdb177c2f02de4ab67fb575457a1d3c
loilm <lemauloi0395@gmail.com>
2022-07-09 13:41:11 +0700

    optimize code

--- src/test/java/commons/CommonTest.java
+++ src/test/java/commons/CommonTest.java
@@ -5,15 +5,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @Listeners(TestListener.class)
 public class CommonTest {
-    private DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
-    private Date date = new Date();
-
     /*Setting browser and run testcase with .xml file*/
     @Parameters("browser")
     @BeforeClass
